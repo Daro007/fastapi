d36c76d4f8400ab1f9dbae8cacb3f68c0dbb401b
dario elias <darioelias@darios-iMac.local>
2023-08-27 17:10:58 +0200

    modified user creation and login

--- main.py
+++ main.py
@@ -26,10 +26,12 @@ async def healthcheck():
 
 fake_users_db = {
     "testuser1": {
+        "email": "testuser1@example.com",
         "username": "testuser1",
         "password": "testpassword1",
     },
     "testuser2": {
+        "email": "testuser2@example.com",
         "username": "testuser2",
         "password": "testpassword2",
     }
@@ -37,12 +39,14 @@ fake_users_db = {
 
 class User(BaseModel):
     username: str
+    email: str
+
+class UserCreate(User):
+    password: str
 
 class UserInDB(User):
     password: str
 
-class UserCreate(User):
-    password: str
 
 # OAuth2 password bearer token
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
@@ -60,6 +64,22 @@ def authenticate_user(username: str, password: str):
         return False
     return True
 
+
+# Route to get a token for authentication
+@app.post("/token")
+async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
+    identifier = form_data.username  # This can be either username or email
+    password = form_data.password
+    user = next((u for u in fake_users_db.values() if u['username'] == identifier or u['email'] == identifier), None)
+    if user is None or user["password"] != password:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username/email or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    return {"access_token": user["username"], "token_type": "bearer"}
+
+
 # Dependency to get the current user
 async def get_current_user(token: str = Depends(oauth2_scheme)):
     credentials_exception = HTTPException(
@@ -73,18 +93,6 @@ async def get_current_user(token: str = Depends(oauth2_scheme)):
         raise credentials_exception
     return user
 
-# Route to get a token for authentication
-@app.post("/token")
-async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
-    username = form_data.username
-    password = form_data.password
-    if not authenticate_user(username, password):
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Incorrect username or password",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
-    return {"access_token": username, "token_type": "bearer"}
 
 # Protected route that requires authentication
 @app.get("/secure-route/")
@@ -108,16 +116,18 @@ async def get_user_by_username(username: str):
     user_dict = fake_users_db[username]
     return UserInDB(**user_dict)
    
-# Create a new user
-@app.post("/users/", response_model=User)
+# Create a user
+@app.post("/users/")
 async def create_user(user: UserCreate):
-    if user.username in fake_users_db:
+    existing_user = next((u for u in fake_users_db.values() if u['username'] == user.username or u['email'] == user.email), None)
+    if existing_user:
         raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail="Username already exists"
+            status_code=400,
+            detail="Username or email already exists"
         )
-    fake_users_db[user.username] = {"username": user.username, "password": user.password}
-    return user
+    user_data = {"username": user.username, "email": user.email, "password": user.password}
+    fake_users_db[user.username] = user_data
+    return user_data
 
 # Modify a user
 @app.put("/users/{username}", response_model=User)
